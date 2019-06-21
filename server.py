f4431976c5d3b2b445ad2c378c38282beb2d32fe
Sumukh Sridhara <sumukh1@gmail.com>
2019-06-21 00:39:42 -0700

    requests.get from weather

--- server.py
+++ server.py
@@ -1,3 +1,5 @@
+import requests
+
 from datetime import datetime
 from flask import Flask, redirect, request, jsonify
 
@@ -17,5 +19,17 @@ def ip():
 def time():
     return jsonify({
         'current_time': datetime.now(),
+    })
+
+@app.route('/weather/<string:city>')
+def weather(city):
+    # Using a test API key here for the sake of example.
+    response = requests.get("https://api.openweathermap.org/data/2.5/weather",
+                            {'q': city, 'appid': '4a48e1e1428fd83889074671fbf259d9'})
+
+    data = response.json()
+    return jsonify({
+        'city': data["name"],
+        'weather': data["weather"][0]["description"],
     })
 
